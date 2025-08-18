f89df81d0630c831b44abe0d36dc41eba2276d61
adriabama06 <adriabama06@gmail.com>
2025-08-18 04:11:33 +0200

    Don't predownload models

--- util/app.indextts.1.5.py
+++ util/app.indextts.1.5.py
@@ -2,8 +2,36 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import base64, tempfile, os
 from indextts.infer import IndexTTS
+from huggingface_hub import hf_hub_download
 
 app = FastAPI()
+
+REPO_ID = "IndexTeam/IndexTTS-1.5"
+FILES = [
+    "config.yaml",
+    "bigvgan_discriminator.pth",
+    "bigvgan_generator.pth",
+    "bpe.model",
+    "dvae.pth",
+    "gpt.pth",
+    "unigram_12000.vocab"
+]
+LOCAL_DIR = "checkpoints"
+
+os.makedirs(LOCAL_DIR, exist_ok=True)
+for file in FILES:
+    file_path = os.path.join(LOCAL_DIR, file)
+    if not os.path.exists(file_path):
+        print(f"Downloading required file: {file}")
+        hf_hub_download(
+            repo_id=REPO_ID,
+            filename=file,
+            local_dir=LOCAL_DIR,
+            local_dir_use_symlinks=False,
+            force_download=False
+        )
+    else:
+        print(f"{file} exist")
 
 class VoiceCloneRequest(BaseModel):
     audio_base64: str
